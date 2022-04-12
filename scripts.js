08a113ede8f9b0fb7a6362190cc2eed5a66169c0
Francielle Soares <frangirassoll@gmail.com>
2022-04-12 15:35:41 -0300

    responsive mobile web

--- scripts.js
+++ scripts.js
@@ -2,4 +2,8 @@ function menuOnClick() {
   document.getElementById("menu-bar").classList.toggle("change");
   document.getElementById("nav").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
+
+  document.getElementById("modal").classList.toggle("modal");
+
+  document.getElementsByTagName("body")[0].classList.toggle("body-overflow-hidden");
 }
